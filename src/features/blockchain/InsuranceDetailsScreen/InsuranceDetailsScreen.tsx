5c9269245ae2f154690470a2715c5d53ccd441b1
Kakkar, shubham <shubham.kakkar98@gmail.com>
2021-01-29 14:44:59 +0530

    dispatch logic added in insurance image as well

--- src/features/blockchain/InsuranceDetailsScreen/InsuranceDetailsScreen.tsx
+++ src/features/blockchain/InsuranceDetailsScreen/InsuranceDetailsScreen.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainContainer from 'UI/MainContainer';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import KeyboardAvoidingViewUI from 'UI/KeyboardAvoidingViewUI';
 import { Formik, FormikHelpers } from 'formik';
 import AnimatedButton from 'UI/Buttons/AnimatedButton';
-import { useRequestDanglingBlockMutation } from 'generated/graphql';
+import { TRequestedDanglingBlock, useRequestDanglingBlockMutation } from 'generated/graphql';
+import { addDanglingBlock, addMyDanglingBlock } from 'store/actions/danglingBlocks.actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserProfile } from 'store/selectors/user.selectors';
 import InsuranceFormFields from './container/InsuranceFormFields';
 import styles from './insuranceDetailsScreen.styles';
 import {
@@ -14,8 +17,28 @@ import {
 } from './insuranceDetailsScreen.formik';
 
 export default function InsuranceDetailsScreen() {
+  const userProfile = useSelector(selectUserProfile);
+  const dispatch = useDispatch();
   const [requestDanglingBlock, requestDanglingBlockResponse] = useRequestDanglingBlockMutation();
-
+  useEffect(() => {
+    if (requestDanglingBlockResponse.data?.requestDanglingBlock) {
+      const block = {
+        ...requestDanglingBlockResponse.data?.requestDanglingBlock,
+        user: {
+          firstName: userProfile.get('firstName'),
+          lastName: userProfile.get('lastName'),
+          middleName: userProfile.get('middleName'),
+        },
+      };
+      dispatch(addDanglingBlock(
+        block as TRequestedDanglingBlock,
+      ));
+      dispatch(addMyDanglingBlock(
+        block as TRequestedDanglingBlock,
+      ));
+      Alert.alert('Success', 'Block requested');
+    }
+  }, [requestDanglingBlockResponse]);
   async function formSubmitHandler(
     value: InitialInsuranceDetailsState,
     helpers: FormikHelpers<InitialInsuranceDetailsState>,
